09086a03f49171b29340c0a76496fcf52336fae8
matchi-1 <misheruvynx@gmail.com>
2026-02-28 17:27:08 +0800

    [recent news mod] converted css to css modules

--- src/modules/RecentNews/RecentNews.jsx
+++ src/modules/RecentNews/RecentNews.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import "./styles/RecentNews.css";
+import styles from "./styles/RecentNews.css";
 
 const BodyContent = () => {
     return (
-        <div className="recentnews">
-            <div className="body-content-container">
+        <div className={styles.recentnews}>
+            <div className={styles.bodyContentContainer}>
                 <p>Hello Recent News Module!</p>
                 <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.recentnews' at the start.</p>
             </div>
         </div>
     );
--- src/modules/RecentNews/submodules/RecentNews-dsh.jsx
+++ src/modules/RecentNews/submodules/RecentNews-dsh.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "../styles/RecentNews-dsh.css";
+import styles from "../styles/RecentNews-dsh.css";
 
 const BodyContent = () => {
   return (
-    <div className="recentnews-dsh">
-      <div className="body-content-container">
+    <div className={styles.recentnewsDsh}>
+      <div className={styles.bodyContentContainer}>
         <p>Hello Recent News Dashboard SubModule!</p>
         <p>Fill this container with your elements, change the display if need be.</p>
         <p>If you're going to style with css, use your unique namespace '.recentnews-dsh' at the start.</p>
