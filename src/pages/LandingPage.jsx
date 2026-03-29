05cc407b5d3c0e4a8fb672df6a7bea71bf0181c9
matchi-1 <misheruvynx@gmail.com>
2026-03-30 01:16:00 +0800

    removed unused img assets

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,10 +4,7 @@ import "./LandingPage.css";
 export default function LandingPage() {
   return (
     <div className="landing-content">
-      <img src="/icons/landing logo.png" alt="Kinetiq Logo" className="landing-logo" />
-      <p className="landing-subtext">
-        Precision medical equipment manufacturer advancing healthcare in the Philippines.<br />
-      </p>
+
     </div>
   );
 }
